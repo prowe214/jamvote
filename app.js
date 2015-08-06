d72858a1cd452006cb0c95b381a55d5bdb583c39
Paul Cullen Rowe <prowe214@gmail.com>
2015-08-06 13:44:12 -0600

    removed unnecessary modules

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -19,7 +17,6 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
