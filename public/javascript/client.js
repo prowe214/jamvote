77d67c330bdbc49a8163686f4d8db0e8da1f6254
Paul Cullen Rowe <prowe214@gmail.com>
2015-08-06 15:03:15 -0600

    PIVOTING

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,3 +1,22 @@
-// var showComment = document.getElementsByClassName('commentsButton')
+// // initialize soundcloud API with key
+// SC.initialize({
+//     // This is the sample client_id. you should replace this with your own
+//     client_id: "887b335a80f3e625454ebca548c53d96"
+// });
 //
-// console.log(showComment)
+// function embed (id) {
+//   SC.oEmbed("http://soundcloud.com/" + id // user or playlist to embed
+//     , { color: "ff0066"
+//       , auto_play: true
+//       , maxwidth: 500
+//       , maxheight: 1000
+//       , show_comments: true } // options
+//     , document.getElementById("scWidget") // what element to attach player to
+//   );
+// }
+//
+// $(document).ready(
+//   function () {
+//     embed("dj-faze");
+//   }
+// );
